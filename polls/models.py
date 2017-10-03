a81564cc79bc8196146c42f11940eb6317177135
Andrei Tcibin <andrei_tcibin@epam.com>
2017-10-03 20:03:21 +0300

    feat: Perform russification to the whole project

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField('Текст вопроса', max_length=200)
+    pub_date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
         return self.question_text
@@ -16,13 +16,21 @@ class Question(models.Model):
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+    was_published_recently.short_description = 'Опубликованно недавно?'
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField('Вариант ответа', max_length=200)
+    votes = models.IntegerField('Кол-во голосов', default=0)
 
     def __str__(self):
         return self.choice_text
+
+    class Meta:
+        verbose_name = 'ответ'
+        verbose_name_plural = 'ответы'
